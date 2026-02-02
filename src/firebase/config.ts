1a415a0710f047129be2dd977dfbaa9632f8e7de
William Tan <williamtyc82@gmail.com>
2026-02-02 01:40:11 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-374816510-d4f3d",
+  "appId": "1:150759802952:web:36172a802cfd5665ff307d",
+  "apiKey": "AIzaSyB__rdJiwW4fKr2PpxY529eXSovUuzWgoI",
+  "authDomain": "studio-374816510-d4f3d.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "150759802952"
+};
